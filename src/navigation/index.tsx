f43b08404dcb0e37d03b56768459aec06fea251c
Mike <mnightingale@tutanota.com>
2020-07-30 20:48:17 +0100

    Improved navbar

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -7,29 +7,71 @@ import {
   Collapse,
   Nav,
   NavLink,
+  NavbarText,
+  Container,
+  Col,
+  Row,
+  Button,
 } from "reactstrap";
 
-export let Navigation = () => {
-  const [isCollapsed, setCollapsed] = React.useState(true);
-  const toggleNavbar = () => setCollapsed(!isCollapsed);
+export const Navigation = () => {
+  const [navIsCollapsed, setNavCollapsed] = React.useState(true);
+  const [peakIsCollapsed, setPeakCollapsed] = React.useState(true);
+  const toggleNavbar = () => setNavCollapsed(!navIsCollapsed);
+  const togglePeak = () => setPeakCollapsed(!peakIsCollapsed);
 
   return (
-    <Navbar color="light" light expand="md">
-      <NavbarBrand href="#">Stopwatch</NavbarBrand>
-      <NavbarToggler onClick={toggleNavbar} className="mr-2"></NavbarToggler>
-      <Collapse isOpen={!isCollapsed} navbar>
-        <Nav navbar>
-          <NavItem>
-            <NavLink href="#">Stopwatch</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Timer</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Clock</NavLink>
-          </NavItem>
-        </Nav>
+    <>
+      {/* Peak */}
+      <Collapse isOpen={!peakIsCollapsed}>
+        <Container className="bg-primary text-light" fluid>
+          <Row>
+            <Col xs="12" md="2">
+              Stopwatch 1
+            </Col>
+            <Col>0:00:00</Col>
+          </Row>
+          <Row>
+            <Col xs="12" md="2">
+              Stopwatch 2
+            </Col>
+            <Col>0:00:00</Col>
+          </Row>
+          <Row>
+            <Col xs="12" md="2">
+              Stopwatch 3
+            </Col>
+            <Col>0:00:00</Col>
+          </Row>
+        </Container>
       </Collapse>
-    </Navbar>
+
+      {/* Navbar */}
+      <Navbar color="primary" dark expand="md" sticky="top">
+        <NavbarBrand href="#">Stopwatch/Timer App</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2"></NavbarToggler>
+        <Collapse isOpen={!navIsCollapsed} navbar>
+          {/* Left content */}
+          <Nav className="mr-auto" navbar>
+            <NavItem active>
+              <NavLink href="#">Stopwatch</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Timer</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Clock</NavLink>
+            </NavItem>
+          </Nav>
+          {/* Right content */}
+          <Nav>
+            <NavbarText>0:00:00</NavbarText>
+            <Button onClick={togglePeak} outline color="link">
+              <span className="dropdown-toggle"></span>
+            </Button>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
   );
 };
