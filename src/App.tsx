efc3cc0a81e7f97a4d7f6e7560fff432ededca7c
Mike <mnightingale@tutanota.com>
2020-08-18 16:18:57 +0100

    Improve routing transition animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import { Navigation } from "./navigation";
@@ -13,26 +13,38 @@ type RouteElement = {
   path: string;
   Component: JSX.Element;
 };
-const routes: RouteElement[] = [
-  { path: "/clock", Component: <Clock /> },
+const routes: readonly RouteElement[] = [
   { path: "/stopwatch", Component: <Stopwatch /> },
   { path: "/timer", Component: <Timer /> },
-  { path: "/", Component: <Clock /> },
+  { path: "/clock", Component: <Clock /> },
 ];
 
 function App() {
   let location = useLocation();
+  let prevLocation = React.useRef(location);
+
+  const routeIndex = (path: string) =>
+    routes.findIndex((route) => route.path === path);
+  const slideDirection =
+    routeIndex(location.pathname) >= routeIndex(prevLocation.current.pathname)
+      ? "left"
+      : "right";
+
+  React.useEffect(() => {
+    prevLocation.current = location;
+  });
 
   return (
     <>
       <Navigation />
 
-      <TransitionGroup className="content">
-        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+      <TransitionGroup className={`main-content slide-${slideDirection}`}>
+        <CSSTransition key={location.key} classNames={`slide`} timeout={300}>
           <Switch location={location}>
             {routes.map((route) => (
               <Route path={route.path}>{route.Component}</Route>
             ))}
+            <Redirect to="/clock" /> // Any other pages redirect to clock
           </Switch>
         </CSSTransition>
       </TransitionGroup>
