9152ed5ef5fbb9797c479735875febaa64bc7ebc
Mike <mnightingale@tutanota.com>
2020-10-20 19:47:30 +0100

    Refactor break navigation bar into components

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -13,18 +13,80 @@ import {
   Row,
   Button,
 } from "reactstrap";
-import { NavigationProps } from "./types";
+import {
+  NavigationProps,
+  NavbarItemProps,
+  NavPeakProps,
+  NavMainBarProps,
+  NavMainBarRightProps,
+} from "./types";
 
-type NavbarItemProps = {
-  to: string;
-  children: any;
-};
 const NavbarItem = ({ to, children }: NavbarItemProps) => (
   <NavItem>
     <NavLink to={to} className="nav-link" activeClassName="active">
       {children}
     </NavLink>
   </NavItem>
+);
+
+const NavPeak = ({ collapsed }: NavPeakProps) => (
+  <Collapse isOpen={!collapsed}>
+    <Container className="bg-primary text-light" fluid>
+      <Row>
+        <Col xs="12" md="2">
+          Stopwatch 1
+        </Col>
+        <Col>0:00:00</Col>
+      </Row>
+      <Row>
+        <Col xs="12" md="2">
+          Stopwatch 2
+        </Col>
+        <Col>0:00:00</Col>
+      </Row>
+      <Row>
+        <Col xs="12" md="2">
+          Stopwatch 3
+        </Col>
+        <Col>0:00:00</Col>
+      </Row>
+    </Container>
+  </Collapse>
+);
+
+const NavMainBarLeft = () => (
+  <Nav className="mr-auto" navbar>
+    <NavbarItem to="/stopwatch">Stopwatch</NavbarItem>
+    <NavbarItem to="/timer">Timer</NavbarItem>
+    <NavbarItem to="/clock">Clock</NavbarItem>
+  </Nav>
+);
+
+const NavMainBarRight = ({ togglePeak, toggleTheme }: NavMainBarRightProps) => (
+  <Nav>
+    <Button onClick={togglePeak} outline color="link">
+      <NavbarText>0:00:00</NavbarText> <span className="dropdown-toggle"></span>
+    </Button>
+    <Button onClick={toggleTheme} outline color="link">
+      <NavbarText>Theme</NavbarText>
+    </Button>
+  </Nav>
+);
+
+const NavMainBar = ({
+  collapsed,
+  toggleCollapse,
+  togglePeak,
+  toggleTheme,
+}: NavMainBarProps) => (
+  <Navbar color="primary" dark expand="md" sticky="top" className="shadow-sm">
+    <NavbarBrand href="#">Stopwatch/Timer App</NavbarBrand>
+    <NavbarToggler onClick={toggleCollapse} className="mr-2"></NavbarToggler>
+    <Collapse isOpen={!collapsed} navbar>
+      <NavMainBarLeft />
+      <NavMainBarRight togglePeak={togglePeak} toggleTheme={toggleTheme} />
+    </Collapse>
+  </Navbar>
 );
 
 export const Navigation = ({ toggleTheme }: NavigationProps) => {
@@ -35,59 +97,13 @@ export const Navigation = ({ toggleTheme }: NavigationProps) => {
 
   return (
     <>
-      {/* Peak */}
-      <Collapse isOpen={!peakIsCollapsed}>
-        <Container className="bg-primary text-light" fluid>
-          <Row>
-            <Col xs="12" md="2">
-              Stopwatch 1
-            </Col>
-            <Col>0:00:00</Col>
-          </Row>
-          <Row>
-            <Col xs="12" md="2">
-              Stopwatch 2
-            </Col>
-            <Col>0:00:00</Col>
-          </Row>
-          <Row>
-            <Col xs="12" md="2">
-              Stopwatch 3
-            </Col>
-            <Col>0:00:00</Col>
-          </Row>
-        </Container>
-      </Collapse>
-
-      {/* Navbar */}
-      <Navbar
-        color="primary"
-        dark
-        expand="md"
-        sticky="top"
-        className="shadow-sm"
-      >
-        <NavbarBrand href="#">Stopwatch/Timer App</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2"></NavbarToggler>
-        <Collapse isOpen={!navIsCollapsed} navbar>
-          {/* Left content */}
-          <Nav className="mr-auto" navbar>
-            <NavbarItem to="/stopwatch">Stopwatch</NavbarItem>
-            <NavbarItem to="/timer">Timer</NavbarItem>
-            <NavbarItem to="/clock">Clock</NavbarItem>
-          </Nav>
-          {/* Right content */}
-          <Nav>
-            <Button onClick={togglePeak} outline color="link">
-              <NavbarText>0:00:00</NavbarText>{" "}
-              <span className="dropdown-toggle"></span>
-            </Button>
-            <Button onClick={toggleTheme} outline color="link">
-              <NavbarText>Theme</NavbarText>
-            </Button>
-          </Nav>
-        </Collapse>
-      </Navbar>
+      <NavPeak collapsed={peakIsCollapsed} />
+      <NavMainBar
+        collapsed={navIsCollapsed}
+        toggleCollapse={toggleNavbar}
+        togglePeak={togglePeak}
+        toggleTheme={toggleTheme}
+      />
     </>
   );
 };
--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,3 +1,24 @@
 export type NavigationProps = {
   toggleTheme: () => void;
 };
+
+export type NavbarItemProps = {
+  to: string;
+  children: any;
+};
+
+export type NavPeakProps = {
+  collapsed: boolean;
+};
+
+export type NavMainBarProps = {
+  collapsed: boolean;
+  toggleCollapse: () => void;
+  togglePeak: () => void;
+  toggleTheme: () => void;
+};
+
+export type NavMainBarRightProps = {
+  togglePeak: () => void;
+  toggleTheme: () => void;
+};
