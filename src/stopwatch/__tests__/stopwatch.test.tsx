e869b07e31ca68691ac8cc69c15079af7c0eb02b
Mike <mnightingale@tutanota.com>
2021-01-22 22:18:38 +0000

    Add stopwatch tests todo

---
+++ src/stopwatch/__tests__/stopwatch.test.tsx
@@ -0,0 +1,22 @@
+import { shallow, ShallowWrapper } from "enzyme";
+import { Stopwatch } from "..";
+
+describe("main functionality", () => {
+    it.todo("renders without error");
+    it.todo("time label and time value render")
+    it.todo("all buttons render correctly")
+    it.todo("time after start button clicked displays correctly");
+    it.todo("time pauses after pause button clicked");
+    it.todo("time resumes when start clicked after paused");
+    it.todo("time resets when reset button is clicked");
+    it.todo("time correctly starts after reset button clicked");
+    it.todo("time pauses at zero when reset is clicked");
+});
+
+describe("laps", () => {
+    it.todo("lap button creates a new lap element");
+    it.todo("lap element renders correctly");
+    it.todo("lap elements enumerate correctly");
+    it.todo("display shows lap time and total time after lap button clicked");
+    it.todo("correct time values show");
+});
